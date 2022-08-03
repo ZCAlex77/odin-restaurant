02edc1ff2cb10462752a9f72e9eb853a3d5935fc
Alex <alexandruzmau320@gmail.com>
2022-08-03 21:03:06 +0300

    Add ability to change tab

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,18 +3,29 @@ import "../styles/nav.css";
 const Nav = () => {
   const nav = document.createElement("nav"),
     links = document.createElement("ul");
-  
-  const handleClick = (thisLink) =>{
-    document.querySelectorAll("nav > ul > li").forEach(link => link.classList.remove('active-link'));
-    thisLink.classList.add('active-link');
-  }
+
+  const handleClick = (thisLink) => {
+    const displays = { about: "block", menu: "grid", contact: "block" };
+    document
+      .querySelectorAll("nav > ul > li")
+      .forEach((link) => link.classList.remove("active-link"));
+    thisLink.classList.add("active-link");
+    document
+      .querySelectorAll("main > div")
+      .forEach((div) => (div.style.display = "none"));
+    document.querySelector(
+      `#${thisLink.getAttribute("data-link")}`
+    ).style.display = displays[thisLink.getAttribute("data-link")];
+  };
 
   ["About", "Menu", "Contact"].forEach((item) => {
     const link = document.createElement("li");
-
+    link.setAttribute("data-link", item.toLowerCase());
     link.textContent = item;
-    link.onclick = function(){handleClick(this)};
-    if(item === "About") link.classList.add('active-link');
+    link.onclick = function () {
+      handleClick(this);
+    };
+    if (item === "About") link.classList.add("active-link");
     links.appendChild(link);
   });
 
