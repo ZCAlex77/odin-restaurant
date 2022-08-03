7881e977a97307be520f721650351a74e1a2936b
Alex <alexandruzmau320@gmail.com>
2022-08-03 21:14:42 +0300

    Create contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,28 @@
+import "../styles/contact.css";
+
+const Contact = () => {
+  const div = document.createElement("div"),
+    ul = document.createElement("ul"),
+    p = document.createElement("p"),
+    h2 = document.createElement("h2");
+  
+  div.id = "contact";
+
+  h2.textContent = "Hey potential customer!";
+  p.textContent =
+    "It seems we made you hungry by showcasing all those amazing dishes. Here are some ways to get your hand on our food:";
+  [
+    "Call us at: 0000 000 000",
+    "Email us at: alexsdishes@awesome.com",
+    "Visit us at: Somewhere",
+  ].forEach((text) => {
+    let li = document.createElement("li");
+    li.textContent = text;
+    ul.appendChild(li);
+  });
+
+  div.append(h2, p, ul);
+  return div;
+};
+
+export default Contact;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Nav from "./components/Nav";
 import About from "./components/About";
 import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 import "./styles/general.css";
 
 const render = () => {
@@ -9,7 +10,7 @@ const render = () => {
     main = document.createElement("main");
 
   container.id = "content";
-  main.append(About(), Menu());
+  main.append(About(), Menu(), Contact());
 
   container.append(Header(), Nav(), main);
   document.body.appendChild(container);
